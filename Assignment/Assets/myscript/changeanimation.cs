132eb90f29282e20dfb8d6123096f01cc2835965
Adam0429 <872490934@qq.com>
2018-10-31 15:41:36 +0800

    begin to do animator

---
+++ Assignment/Assets/myscript/changeanimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeanimation : MonoBehaviour {
+    public GameObject samuzai;
+    Animator animator;
+    // Use this for initialization
+    void Start () {
+        samuzai = GameObject.Find("samuzai");
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    public void buttonattach(){
+        animator.SetTrigger("attach");      
+    }
+
+    public void buttonrun()
+    {
+        animator.SetTrigger("run");
+    }
+}
